2dbd894f906b2d7cf4cb043563453b039a8dfa20
Julien <48369040+julien040@users.noreply.github.com>
2023-06-16 13:20:32 +0200

    ✨ Add table of content to article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import remarkToc from 'remark-toc'
+import remarkBreaks from "remark-breaks";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkToc, remarkBreaks],
+  },
 });
